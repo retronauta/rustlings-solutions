c1916b47f26760e55c303960fdb9afd74da9acd5
Limbert Lino <limbertlino@gmail.com>
2023-10-12 11:55:17 -0400

    Done if block

--- if/if3.rs
+++ if/if3.rs
@@ -2,17 +2,18 @@
 //
 // Execute `rustlings hint if3` or use the `hint` watch subcommand for a hint.
 
-// I AM NOT DONE
+
+//* In Rust, every arm of an `if` expression has to return the same type of value. Make sure the type is consistent across all arms. */
 
 pub fn animal_habitat(animal: &str) -> &'static str {
     let identifier = if animal == "crab" {
         1
     } else if animal == "gopher" {
-        2.0
+        2
     } else if animal == "snake" {
         3
-    } else {
-        "Unknown"
+    }else {
+        0
     };
 
     // DO NOT CHANGE THIS STATEMENT BELOW
