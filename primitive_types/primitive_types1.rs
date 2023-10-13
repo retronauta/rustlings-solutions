b7cd6af03efb84d928e82fd019db43b33c5c8c52
Limbert Lino <limbertlino@gmail.com>
2023-10-13 08:43:03 -0400

    Done primitive_types section

--- primitive_types/primitive_types1.rs
+++ primitive_types/primitive_types1.rs
@@ -3,17 +3,17 @@
 // Fill in the rest of the line that has code missing! No hints, there's no
 // tricks, just get used to typing these :)
 
-// I AM NOT DONE
 
+//* The boolean type is declared: bool
 fn main() {
     // Booleans (`bool`)
-
-    let is_morning = true;
+    let is_morning: bool = true;
     if is_morning {
         println!("Good morning!");
     }
 
-    let // Finish the rest of this line like the example! Or make it be false!
+// Finish the rest of this line like the example! Or make it be false!
+let is_evening: bool = false;
     if is_evening {
         println!("Good evening!");
     }
